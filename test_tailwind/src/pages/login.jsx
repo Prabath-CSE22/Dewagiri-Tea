2ba41c0b717912e01c8fb2f4b3ae2a9bd132918b
Prabath Samarasinghe <prabhaths2001@gmail.com>
2025-01-04 23:04:41 +0530

    Update login.jsx

--- test_tailwind/src/pages/login.jsx
+++ test_tailwind/src/pages/login.jsx
@@ -6,7 +6,7 @@ const login = () => {
       <body className="w-[90%] h-screen mx-auto bg-gray-200">
         <div className="bg-white w-[35%] my-auto mx-auto p-5 rounded-md shadow-lg shadow-blue-100 relative top-1/2 transform -translate-y-1/2 text-center">
           <i class='bx bx-user text-8xl mx-auto relative top-1/2 transform translate-y-1/3 text-blue-500'></i>
-          <h3 className="text-4xl text-center pt-10">Welcome Back</h3>
+          <p className="xl:text-4xl text-center pt-10 sm:text-2xl">Welcome Back</p>
           <p className='text-xs text-gray-400 '>Please enter your login details</p>
           <form className="flex flex-col justify-center items-center mt-10" onSubmit={(e) => e.preventDefault()}>
             <div className='flex flex-col w-full mx-auto p-2 content-center items-center -gap-0.5'>
